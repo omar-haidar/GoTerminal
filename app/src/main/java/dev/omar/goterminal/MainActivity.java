c91fed5a2051c6bf8a1d4f91a55a7e9f05180fc0
omar-haidar <rovox2000@gmail.com>
2026-07-25 15:38:47 +0300

    first commit

--- app/src/main/java/dev/omar/goterminal/MainActivity.java
+++ app/src/main/java/dev/omar/goterminal/MainActivity.java
@@ -2,6 +2,8 @@ package dev.omar.goterminal;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
+
 import dev.omar.goterminal.databinding.ActivityMainBinding;
 import dev.omar.goterminal.ui.base.EdgeToEdgeActivity;
 import dev.omar.goterminal.utils.UiUtils;
@@ -14,8 +16,21 @@ public class MainActivity extends EdgeToEdgeActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        UiUtils.addSystemWindowInsetToPadding(binding.layoutMain,true,true,true,true);
-        UiUtils.addSystemWindowInsetToPadding(binding.layoutDrawer,true,true,true,true);
-
+        setupLayoutInsets();
+        setupToolbar();
     }
+
+    private void setupLayoutInsets() {
+        UiUtils.addSystemWindowInsetToPadding(binding.layoutMain, true, true, true, true);
+        UiUtils.addSystemWindowInsetToPadding(binding.layoutDrawer, true, true, true, true);
+    }
+
+    private void setupToolbar() {
+        setSupportActionBar(binding.includeToolbar.toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, binding.drawerLayout, binding.includeToolbar.toolbar, R.string.app_name, R.string.app_name);
+        binding.drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+
 }
